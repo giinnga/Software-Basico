989e567a87a6ad152dd2aca0be8abc6b24dbef07
VICTOR AUGUSTO LIMA LINS DE SOUZA <u1310784@jordao.grad.inf.puc-rio.br>
2014-08-19 20:15:49 -0300

    aula 2

--- aula2/string2num.c
+++ aula2/string2num.c
@@ -1,17 +1,18 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int string2num (char *s) {
+int string2num (char *s,int base) {
 	int a = 0;
-	for (; *s; s++) //*s -> *s!=0
-		a = a*10 + (*s - '0');
+	for (; *s; s++){ //*s -> *s!=0
+		a = a*base + (*s - '0');
+		printf("a: %d / s: %c\n",a, *s);
+	}
 	return a;
 }
 
 int main (void) {
-	printf("==> %d\n", string2num("1234"));
-	printf("==> %d\n", string2num("1234") + 1);
-	printf("==> %d\n", string2num("1234") + string2num("1"));
-	printf("%d\n",isdigit(1));
+	printf("%d\n", string2num("777", 8));
+	printf("%d\n", string2num("777", 10));
+	printf("%d\n", isdigit('1'));
 	return 0;
 }
---
+++ aula2/string2numBases.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int string2num (char *s,int base) {
+	int a = 0;
+	for (; *s; s++){ //*s -> *s!=0
+		if (isdigit(*s))
+			a = a*base + (*s - '0');				
+		else
+			a = *s - 'a' + 10;	
+		printf("a: %d / s: %c\n",a, *s);
+	}
+	return a;
+}
+
+int main (void) {
+	  printf("%d\n", string2num("1a", 16));
+  printf("%d\n", string2num("a09b", 16));
+  printf("%d\n", string2num("z09b", 36));
+	return 0;
+}
