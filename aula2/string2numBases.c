eb89a17a45f5c71561d5c7c9dd5b286f3469d0fa
Victor <victoraugustolls@gmail.com>
2014-08-19 21:07:35 -0300

    Trabalho entregue

--- aula2/string2numBases.c
+++ aula2/string2numBases.c
@@ -7,15 +7,15 @@ int string2num (char *s,int base) {
 		if (isdigit(*s))
 			a = a*base + (*s - '0');				
 		else
-			a = *s - 'a' + 10;	
+			a = a*base + *s - 'a' + 10;	
 		printf("a: %d / s: %c\n",a, *s);
 	}
 	return a;
 }
 
 int main (void) {
-	  printf("%d\n", string2num("1a", 16));
-  printf("%d\n", string2num("a09b", 16));
-  printf("%d\n", string2num("z09b", 36));
+	printf("%d\n", string2num("1a", 16));
+	printf("%d\n", string2num("a09b", 16));
+	printf("%d\n", string2num("z09b", 36));
 	return 0;
 }
