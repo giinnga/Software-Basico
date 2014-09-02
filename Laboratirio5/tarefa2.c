a8c77e890fd25e4f813859efaab6ffc3e66f00b1
Victor <victoraugustolls@gmail.com>
2014-09-02 18:11:38 -0300

    Lab 5 final

--- Laboratirio5/tarefa2.c
+++ Laboratirio5/tarefa2.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct X {
+struct X {
   int a;
   int b;
   short c;
   int d;
-} x;
+} x = {0xa1a2a3a4, 0xb1b2b3b4, 0xc1c2, 0xd1d2d3d4};
 
 void dump (void *p, int n) {
   unsigned char *p1 = p;
@@ -16,10 +17,7 @@ void dump (void *p, int n) {
 }
 
 int main (void) {
-
-	printf ("b: \n");
-	dump (b, sizeof(b));
-	printf ("a: \n");
-	dump (a, sizeof(a));
+	printf ("x: \n");
+	dump (&x, sizeof(x));
 	return 0;
 }
